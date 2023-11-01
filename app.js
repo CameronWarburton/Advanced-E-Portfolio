dea05b2e64a48bf43cd6d82bfa1c110b810f1925
Cameron <cameron.cw@outlook.com>
2023-11-01 13:51:46 +1000

    Contact Modal work

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading')
     const success = document.querySelector('.modal__overlay--success')
-    loading.classList += " modal__overlay--visilbe";
+    loading.classList += " modal__overlay--visible";
     emailjs
     .sendForm(
         'service_i7xlhij',
@@ -14,12 +14,23 @@ function contact(event) {
         event.target,
         'Rl_7SXYFQZ8JlOrCE'
     ).then(() => {
-        loading.classList.remove("modal__overlay--visilbe");
-        success.classList += " modal__overlay--visilbe";
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
     }).catch(() => {
-        loading.classList.remove("modal__overlay--visilbe");
+        loading.classList.remove("modal__overlay--visible");
         alert(
             "The email service is temporarly unavailable. Please contact me directly on Cameron.cw@outlook.com"
         );
     })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+        document.body.classList += " modal--open"
+
 }
